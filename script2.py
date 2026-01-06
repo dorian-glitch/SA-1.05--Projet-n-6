5d76908bb6f1240d7174855dc1fbcbe24fd257c8
dorian-glitch <dorian@ripoche.org>
2026-01-06 17:23:25 +0100

    Add files via upload

---
+++ script2.py
@@ -0,0 +1,47 @@
+import csv
+import numpy as np
+from matplotlib import pyplot as plt
+
+# 1. Chargement des données
+donnees = []
+with open('_C3_89volution_du_parc_install_C3_A9_de_production_d_27_C3_A9lectricit_C3_A9_en_France_2025-12-18_13-39.csv', newline='', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile, delimiter=';')
+    next(reader)  # Saute l'en-tête
+    for row in reader:
+        if len(row) >= 3 and row[2] != '':
+            donnees.append(row)
+dates = []
+solaire_pop = []
+thermique_pop = []
+
+for row in donnees:
+    annee = int(row[0])
+    filiere = row[1]
+    valeur = float(row[2].replace(',', '.'))
+
+    if filiere == "Solaire":
+        if annee not in dates:
+            dates.append(annee)
+        solaire_pop.append(valeur)
+    
+    if filiere == "Thermique fossile":
+        thermique_pop.append(valeur)
+plt.scatter(dates, solaire_pop, color='orange', label='Solaire')
+plt.scatter(dates, thermique_pop, color='grey', label='Thermique fossile')
+
+dates_projection = [2007, 2030] 
+
+coef_sol = np.polyfit(dates, solaire_pop, 1)
+poly_sol = np.poly1d(coef_sol)
+plt.plot(dates_projection, poly_sol(dates_projection), color='orange', linestyle='dashed')
+coef_ther = np.polyfit(dates, thermique_pop, 1)
+poly_ther = np.poly1d(coef_ther)
+plt.plot(dates_projection, poly_ther(dates_projection), color='grey', linestyle='dashed')
+
+
+plt.title("Évolution de la puissance installée en France (GW)")
+plt.ylabel('Puissance (GW)')
+plt.xlabel('Années')
+plt.legend()
+
+plt.show()
